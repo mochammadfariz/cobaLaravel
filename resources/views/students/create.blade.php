4604d0f59323446158d02d0a8681c0879d09678e
mochammadfariz <mochammadfariz11@gmail.com>
2019-08-19 14:02:36 +0700

    menambahkan old input pada kolom agar value tidak hilang

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -12,25 +12,25 @@
       @csrf
         <div class="form-group">
             <label for="nama">Nama</label>
-            <input name="nama" type="text" class="form-control @error('nama')is-invalid @enderror" id="nama" placeholder="Masukkan Nama">
+            <input name="nama" type="text" class="form-control @error('nama')is-invalid @enderror" id="nama" placeholder="Masukkan Nama" value="{{old('nama')}}">
             @error('nama')
                 <div class="invalid-feedback" > {{$message}}</div>
             @enderror
         </div>
        <div class="form-group">
             <label for="nrp">Nrp</label>
-            <input name="nrp" type="text" class="form-control  @error('nrp')is-invalid @enderror" id="nrp" placeholder="Masukkan NRP">
+            <input name="nrp" type="text" class="form-control  @error('nrp')is-invalid @enderror" id="nrp" placeholder="Masukkan NRP" value="{{old('nrp')}}">
             @error('nrp')
                 <div class="invalid-feedback" > {{$message}}</div>
             @enderror
         </div>
         <div class="form-group">
             <label for="email">Email</label>
-            <input name="email" type="text" class="form-control" id="email" placeholder="Masukkan Email">
+            <input name="email" type="text" class="form-control" id="email" placeholder="Masukkan Email" value="{{old('email')}}">
         </div>
         <div class="form-group">
             <label for="jurusan">Jurusan</label>
-            <input name="jurusan" type="text" class="form-control" id="jurusan" placeholder="Masukkan Jurusan">
+            <input name="jurusan" type="text" class="form-control" id="jurusan" placeholder="Masukkan Jurusan" value="{{old('jurusan')}}">
         </div>
         <button type="submit" class="btn btn-primary">Tambah Data</button>
     </form>
